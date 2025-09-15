1b9dbbb3af31bdcdcd192532756b30d0c836d71c
idil <idili@hotmail.co.uk>
2025-09-15 18:23:39 +0100

    Implement code changes for improved functionality and performance enhancements

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path('posts/', views.PostList.as_view(), name='home'),    # homepage
+    path('<slug:slug>/', views.post_detail, name='post_detail'),    # detail page for each post
   
 ]
