5ed53f03cc1d196a628432deea6e381775c6ccf4
idil <idili@hotmail.co.uk>
2025-09-15 18:42:58 +0100

    Fix comment for clarity in post detail URL pattern

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path('posts/', views.PostList.as_view(), name='home'),    # homepage
-    path('<slug:slug>/', views.post_detail, name='post_detail'),    # detail page for each post
+    path('<slug:slug>/', views.post_detail, name='post_detail'),    # Single post detail
   
 ]
