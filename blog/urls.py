5c93cdbfbf29a8683ed30fa7407ba0317373f70e
idil <idili@hotmail.co.uk>
2025-09-16 13:49:52 +0100

    Fix URL patterns: update homepage path and include necessary imports

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,10 @@
-from .import views
-from django.urls import path
+
+from django.urls import path, include
+from blog import views
 
 urlpatterns = [
-    path('posts/', views.PostList.as_view(), name='home'),    # homepage
-    path('<slug:slug>/', views.post_detail, name='post_detail'),    # Single post detail
-  
+    path('admin/', admin.site.urls),
+    path('summernote/', include('django_summernote.urls')),
+    path('', views.PostList.as_view(), name='home'),  # root URL shows PostList
+    path('<slug:slug>/', views.post_detail, name='post_detail'),
 ]
