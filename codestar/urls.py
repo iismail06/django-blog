3996efc44e6a67eeff2c6fc990be43023f3f40cb
idil <idili@hotmail.co.uk>
2025-09-16 10:13:29 +0100

    Add event detail view and update URL configuration

--- codestar/urls.py
+++ codestar/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from blog import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
     path("", include('blog.urls'), name = "blog-urls"),
+    path("events/<int:event_id>/", views.event_detail, name="event_detail")
 
 ]
--- events/views.py
+++ events/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views import generic
+from .models import Event
 
 # Create your views here.
+class EventsList(generic.ListView):
+
+
+    model = Event
+    template_name = "index.html"
+    paginate_by = 12
+
+
+def event_detail(request, event_id):
+
+    # Database request
+    queryset = Event.objects.all()
+    event = get_object_or_404(queryset, event_id=event_id)
+
+    return render(
+        request,
+        "events/event_detail.html",
+        {"event": event}
+    )
