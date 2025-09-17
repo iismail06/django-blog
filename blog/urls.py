fdd912bf4e65d5efde5c6567ba7aa6809ac908af
idil <idili@hotmail.co.uk>
2025-09-17 12:54:38 +0100

    Add comment editing functionality with success/error messages

--- blog/urls.py
+++ blog/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('summernote/', include('django_summernote.urls')),
     path('', views.PostList.as_view(), name='home'),  # root URL shows PostList
     path('<slug:slug>/', views.post_detail, name='post_detail'),
+    path('<slug:slug>/edit_comment/<int:comment_id>', views.comment_edit, name='comment_edit'),
 ]
