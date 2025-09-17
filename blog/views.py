dd227306aef0e61f897b1b8c20346f0a25b8f928
idil <idili@hotmail.co.uk>
2025-09-17 13:07:56 +0100

    Refactor comment_edit view to improve readability and add rendering for GET requests

--- blog/views.py
+++ blog/views.py
@@ -61,23 +61,44 @@ def post_detail(request, slug):
     )
         # return HttpResponseRedirect(reverse('post_detail', args=[slug]))  # Redirect to the same post detail page after POST
     
-    def comment_edit(request, slug, comment_id):
-        """
-        view to edit comments
-        """
-        if request.method == "POST":
-            queryset = Post.objects.filter(status=1)
-            post = get_object_or_404(queryset, slug=slug)
-            comment = get_object_or_404(Comment, pk=comment_id)
-            comment_form = CommentForm(data=request.POST, instance=comment)
-    
-            if comment_form.is_valid() and comment.author == request.user:
-                comment = comment_form.save(commit=False)
-                comment.post = post
-                comment.approved = False
-                comment.save()
-                messages.add_message(request, messages.SUCCESS, 'Comment Updated!')
-            else:
-                messages.add_message(request, messages.ERROR, 'Error updating comment!')
-    
-        return HttpResponseRedirect(reverse('post_detail', args=[slug]))
+def comment_edit(request, slug, comment_id):
+    """
+    View to edit comments
+    """
+    queryset = Post.objects.filter(status=1)
+    post = get_object_or_404(queryset, slug=slug)
+    comment = get_object_or_404(Comment, pk=comment_id)
+    if request.method == "POST":
+        comment_form = CommentForm(data=request.POST, instance=comment)
+        if comment_form.is_valid() and comment.author == request.user:
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.approved = False
+            comment.save()
+            messages.add_message(request, messages.SUCCESS, 'Comment Updated!')
+            return HttpResponseRedirect(reverse('post_detail', args=[slug]))
+    else:
+        comment_form = CommentForm(instance=comment)
+    if request.method == "POST":
+        comment_form = CommentForm(data=request.POST, instance=comment)
+        if comment_form.is_valid() and comment.author == request.user:
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.approved = False
+            comment.save()
+            messages.add_message(request, messages.SUCCESS, 'Comment Updated!')
+            return HttpResponseRedirect(reverse('post_detail', args=[slug]))
+        else:
+            messages.add_message(request, messages.ERROR, 'Error updating comment!')
+            return HttpResponseRedirect(reverse('post_detail', args=[slug]))
+    else:
+        comment_form = CommentForm(instance=comment)
+        return render(
+            request,
+            "blog/comment_edit.html",
+            {
+                "post": post,
+                "comment": comment,
+                "comment_form": comment_form,
+            }
+        )
